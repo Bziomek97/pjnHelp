a803ed1cea8f13f26b0921426d90e208947ece57
Bziomek97 <B.ziomek97@gmail.com>
2021-12-19 17:51:24 +0100

    xd

--- pages/api/watson/hello.js
+++ pages/api/watson/hello.js
@@ -4,16 +4,17 @@ import { IamAuthenticator } from 'ibm-watson/auth'
 const assistant = new AssistantV2({
   version: '2021-11-27',
   authenticator: new IamAuthenticator({
-    apikey: 'b9UWBr9m-4wI2WcEghU8AN68s_4i55a2Cml_wXMZJycr'
+    apikey: 'BDpwt6m68XDEL7i7RK6_kpS05xCwvCf-goKvo1EqgR3H'
   }),
-  serviceUrl: process.env.SERVICE_URL
+  serviceUrl: 'https://api.eu-de.assistant.watson.cloud.ibm.com',
+  disableSslVerification: true,
 })
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
       const results = await assistant.message({
-        assistantId: process.env.ASSISTANT_ID,
+        assistantId: 'dc1ed0bb-bb48-41ce-bd4c-dd7beda5e99d',
         sessionId: req.body.sessionId,
         input: {
           'message_type': 'text',
--- pages/index.js
+++ pages/index.js
@@ -7,9 +7,10 @@ import { IamAuthenticator } from 'ibm-watson/auth'
 const assistant = new AssistantV2({
   version: '2021-11-27',
   authenticator: new IamAuthenticator({
-    apikey: 'b9UWBr9m-4wI2WcEghU8AN68s_4i55a2Cml_wXMZJycr'
+    apikey: 'BDpwt6m68XDEL7i7RK6_kpS05xCwvCf-goKvo1EqgR3H'
   }),
-  serviceUrl: process.env.SERVICE_URL
+  serviceUrl: 'https://api.eu-de.assistant.watson.cloud.ibm.com',
+  disableSslVerification: true,
 })
 
 const sendMsg = async (e, value, sessionId, messageArray, setMessageArray, setValue) => {
@@ -79,7 +80,7 @@ export default function Home({sessionId}) {
 
 export async function getServerSideProps(context) {
   try {
-    const result = await assistant.createSession({assistantId: process.env.ASSISTANT_ID})
+    const result = await assistant.createSession({assistantId: 'dc1ed0bb-bb48-41ce-bd4c-dd7beda5e99d'})
     return {
       props: {
         sessionId: result.result.session_id
